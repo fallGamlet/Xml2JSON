6476aafc3d42c04065b6656976b4061e58411b84
FallGamlet <fallGamlet@yandex.ru>
2016-08-04 11:11:08 +0300

    Update Xml2JSON.java
    1. Rename method searchItem to searchItemWithName
    2. add method searchItemFromField
    3. add method makeShortWithoutAttr

--- Xml2JSON.java
+++ Xml2JSON.java
@@ -1,12 +1,18 @@
+package ru.hellix.fallgamlet.fitness.data;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Created by fallgamlet on 04.07.16.
+ */
 public class Xml2JSON {
     public static final String NODE_FILED_ATTR = "attr";
     public static final String NODE_FILED_NAME = "name";
@@ -86,23 +92,36 @@ public class Xml2JSON {
     }
 
     /**
-     * Get object from src JSON by 'name' field
+     * Get object from src JSON by 'name' field (recursive)
      * @param src Source JSONObject
-     * @param name name of object's tag
-     * @return Founded object or null if not found
+     * @param name Value of object's tag "name"
+     * @return Founded object or null
      */
-    public static Object searchItem(JSONObject src, String name) {
+    public static Object searchItemWithName(JSONObject src, String name) {
         if (src == null) { return null; }
         String curName = src.optString(NODE_FILED_NAME, null);
+        if (curName == null) {
+            Iterator<String> keys = src.keys();
+            if (keys == null || !keys.hasNext()) { return null; }
+            while (keys.hasNext()) {
+                String key = keys.next();
+                boolean check = (key == name)
+                        || (key != null && key.equalsIgnoreCase(name));
+                if (check) { return src.opt(key);}
+            }
+        }
+
         boolean check = (curName == name)
                 || (curName != null && curName.equalsIgnoreCase(name));
         if (check) { return src;}
 
         Object content = src.opt(NODE_FILED_CONTENT);
-        if (content instanceof JSONObject) {
+        if (content == null) {
+            return null;
+        } else if (content instanceof JSONObject) {
             JSONObject jsonContent = (JSONObject)content;
 
-            Object resultObj = jsonContent.optJSONObject(name);
+            Object resultObj = jsonContent.opt(name);
             if (resultObj != null) { return resultObj; }
 
             Iterator<String> keys = jsonContent.keys();
@@ -111,7 +130,7 @@ public class Xml2JSON {
                 Object contentItem = jsonContent.opt(key);
                 if (contentItem != null) {
                     if (contentItem instanceof JSONObject) {
-                        resultObj = searchItem((JSONObject)contentItem, name);
+                        resultObj = searchItemWithName((JSONObject)contentItem, name);
                         if (resultObj != null) { return resultObj; }
                     } else if (contentItem instanceof JSONArray) {
                         JSONArray arr = (JSONArray) contentItem;
@@ -119,7 +138,7 @@ public class Xml2JSON {
                         for (int i=0; i<count; i++) {
                             Object arrItem = arr.opt(i);
                             if (arrItem instanceof JSONObject) {
-                                resultObj = searchItem((JSONObject)arrItem, name);
+                                resultObj = searchItemWithName((JSONObject)arrItem, name);
                                 if (resultObj != null) { return resultObj; }
                             }
                         }
@@ -128,5 +147,254 @@ public class Xml2JSON {
             }
         }
         return null;
+    }
+
+    /**
+     * Get the object that is to be under the specified field
+     * @param src Source JSONObject or JSONArray object for start search (non recursive)
+     * @param fieldName Field name for search
+     * @return Founded object or null
+     */
+    public static Object searchItemFromField(Object src, String fieldName) {
+        if (src == null || !(src instanceof JSONObject || src instanceof JSONArray) || fieldName == null) {
+            return null;
+        }
+
+        LinkedList<Object> openList = new LinkedList<>();
+        openList.addLast(src);
+
+        while (!openList.isEmpty()) {
+            Object oItem = openList.pollFirst();
+            if (oItem instanceof JSONArray) {
+                JSONArray jArr = (JSONArray) oItem;
+                int len = jArr.length();
+                for(int i=0; i<len; i++) {
+                    Object obj = jArr.opt(i);
+                    if (obj != null && (obj instanceof JSONArray || obj instanceof JSONObject)) {
+                        openList.addLast(obj);
+                    }
+                }
+            } else if (oItem instanceof JSONObject) {
+                JSONObject jItem = (JSONObject) oItem;
+                Iterator<String> keys = jItem.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    Object obj = jItem.opt(key);
+                    if (key != null && key.equalsIgnoreCase(fieldName)) {
+                        return obj;
+                    } else if (obj != null && (obj instanceof JSONArray || obj instanceof JSONObject)) {
+                        openList.addLast(obj);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private static Object searchItem(JSONObject src, String filedName, String fieldValue) {
+        if (src == null) {
+            return null;
+        }
+
+        //        String curName = src.optString(NODE_FILED_NAME, null);
+//        if (curName != null) {
+//            if (curName == name || curName.equalsIgnoreCase(name)) {
+//                return src;
+//            }
+//        }
+//
+//        ArrayList<String> keyList = new ArrayList<>();
+//        Iterator<String> keys = src.keys();
+//        if (keys == null || !keys.hasNext()) { return null; }
+//        while (keys.hasNext()) {
+//            keyList.add(keys.next());
+//        }
+//
+//        for (String key : keyList) {
+//            if (key == name || (key != null && key.equalsIgnoreCase(name))) {
+//                return src.opt(key);
+//            }
+//        }
+//
+//        for (String key : keyList) {
+//            Object oItem = src.opt(key);
+//            if (oItem instanceof JSONObject) {
+//                return searchItemWithName((JSONObject)oItem, name);
+//            } else if (oItem instanceof JSONArray) {
+//                JSONArray arr = (JSONArray) oItem;
+//                int count = arr.length();
+//                for (int i=0; i<count; i++) {
+//                    Object arrItem = arr.opt(i);
+//                    if (arrItem instanceof JSONObject) {
+//                        Object resultObj = searchItemWithName((JSONObject)arrItem, name);
+//                        if (resultObj != null) { return resultObj; }
+//                    }
+//                }
+//            }
+//        }
+        return null;
+    }
+
+    /**
+     * Simple convert JSON object to XML string without attributes. Every key it is tag,every value of key is content
+     * @param jObj Src JSON object? must have value types only one of String, JSONObject, JSONArray
+     * @return String with xml text
+    */
+    public static String getXmlString(JSONObject jObj) {
+        if (jObj == null) { return null; }
+        StringBuilder builder = new StringBuilder();
+
+
+        Iterator<String> keys = jObj.keys();
+        while (keys.hasNext()) {
+            String paramName = keys.next();
+            Object oVal = jObj.opt(paramName);
+
+            if (oVal == null) {
+                builder.append("<"); builder.append(paramName); builder.append(">");
+                builder.append("</"); builder.append(paramName); builder.append(">");
+            } else if (oVal instanceof JSONArray) {
+                JSONArray jArr = (JSONArray) oVal;
+                int len = jArr.length();
+                for (int i = 0; i < len; i++) {
+                    Object jArrObjItem = jArr.opt(i);
+                    if (jArrObjItem != null) {
+                        builder.append("<"); builder.append(paramName); builder.append(">");
+                        if (jArrObjItem instanceof JSONObject) {
+                            JSONObject jVal = (JSONObject) jArrObjItem;
+                            String sVal = getXmlString(jVal);
+                            builder.append(sVal == null ? "" : sVal);
+                        } else if (jArrObjItem instanceof String) {
+                            builder.append((String)jArrObjItem);
+                        }
+                        builder.append("</"); builder.append(paramName); builder.append(">");
+                    }
+                }
+            } else {
+                builder.append("<"); builder.append(paramName); builder.append(">");
+                if (oVal instanceof JSONObject) {
+                    JSONObject jVal = (JSONObject) oVal;
+                    String sVal = getXmlString(jVal);
+                    builder.append(sVal == null ? "" : sVal);
+                } else if (oVal instanceof String) {
+                    builder.append((String) oVal);
+                }
+                builder.append("</"); builder.append(paramName); builder.append(">");
+            }
+        }
+        return builder.toString();
+    }
+
+//    /**
+//     * Make JSON object shorter with remove subfiled if subfield value is not JSONObject or JSONArray and remove attributes (attr)
+//     * @param src Source for making short
+//     * @return shorted JSON or null
+//     */
+    public static Object makeShortWithoutAttr(Object src) throws JSONException {
+        if (src == null) { return null; }
+        if (src instanceof JSONObject) {
+            JSONObject jSrc = (JSONObject) src;
+            String name = jSrc.optString(NODE_FILED_NAME, null);
+            Object content = jSrc.opt(NODE_FILED_CONTENT);
+
+            if (name == null) {
+                Iterator<String> keys = jSrc.keys();
+                if (!keys.hasNext()) { return null; }
+
+                JSONObject jRes = new JSONObject();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    Object item = jSrc.opt(key);
+                    if (item != null) {
+                        Object shortedItem = makeShort(item);
+                        jRes.putOpt(key, shortedItem);
+                    }
+                }
+                return jRes;
+            }
+
+            if (content == null) { return null; }
+            Object shortedItem = makeShort(content);
+            if (shortedItem == null) { return null; }
+            return shortedItem;
+
+        } else if (src instanceof JSONArray) {
+            JSONArray jarrSrc = (JSONArray) src;
+            int count = jarrSrc.length();
+            if (count == 0) { return null; }
+            JSONArray jarrRes = new JSONArray();
+            for (int i=0; i<count; i++) {
+                Object oItem = jarrSrc.opt(i);
+                if (oItem != null) {
+                    Object shortedItem = makeShort(oItem);
+                    if (shortedItem != null) { jarrRes.put(shortedItem); }
+                }
+            }
+            return jarrRes;
+        }
+        return src;
+    }
+
+    public static Object makeShort(Object src) throws JSONException {
+        if (src == null) { return null; }
+        if (src instanceof JSONObject) {
+            JSONObject jSrc = (JSONObject) src;
+            String name = jSrc.optString(NODE_FILED_NAME, null);
+//            Object attr = jSrc.opt(NODE_FILED_ATTR);
+            Object content = jSrc.opt(NODE_FILED_CONTENT);
+
+            if (name == null) {
+                Iterator<String> keys = jSrc.keys();
+                if (!keys.hasNext()) { return null; }
+
+                JSONObject jRes = new JSONObject();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    Object item = jSrc.opt(key);
+                    if (item != null) {
+                        Object shortedItem = makeShort(item);
+                        jRes.putOpt(key, shortedItem);
+                    }
+                }
+                return jRes;
+            }
+
+//            if (attr != null) {
+//                JSONObject jRes = new JSONObject();
+//                jRes.putOpt(NODE_FILED_NAME, name);
+//                jRes.putOpt(NODE_FILED_ATTR, attr);
+//                if (content != null) {
+//                    Object shortedItem = makeShort(content);
+//                    if (shortedItem != null) { jRes.putOpt(NODE_FILED_CONTENT, shortedItem); }
+//                }
+//                return jRes;
+//            }
+
+            if (content == null) { return null; }
+            Object shortedItem = makeShort(content);
+            if (shortedItem == null) { return null; }
+//            if (shortedItem instanceof JSONObject || shortedItem instanceof JSONArray) {
+//                JSONObject jRes = new JSONObject();
+//                jRes.putOpt(NODE_FILED_NAME, name);
+//                jRes.putOpt(NODE_FILED_CONTENT, shortedItem);
+//                return jRes;
+//            }
+            return shortedItem;
+
+        } else if (src instanceof JSONArray) {
+            JSONArray jarrSrc = (JSONArray) src;
+            int count = jarrSrc.length();
+            if (count == 0) { return null; }
+            JSONArray jarrRes = new JSONArray();
+            for (int i=0; i<count; i++) {
+                Object oItem = jarrSrc.opt(i);
+                if (oItem != null) {
+                    Object shortedItem = makeShort(oItem);
+                    if (shortedItem != null) { jarrRes.put(shortedItem); }
+                }
+            }
+            return jarrRes;
+        }
+        return src;
     }
 }
